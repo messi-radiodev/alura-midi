8484d25d3764d524be594917903eec66ba7f5b60
Gabriel Messias <gabrielmessias@outlook.com>
2025-05-21 10:41:12 -0300

    projeto midi finalizado

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    // retirado o elemento === null, pois o js já entende se o elemento for nulo, não definido... apenas com o elemento
+    if (elemento && elemento.localName === "audio") {
+        elemento.play(); 
+    } else {
+        console.log ("Elemento não encontrado ou seletor inválido.");
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
 /* estrutura de repetição while
+let contador = 0;
 while (contador < listaDeTeclas.length) {
 
     const tecla = listaDeTeclas[contador];
@@ -29,8 +35,21 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
   
-    tecla.onclick = function () { // função anonima  
+// função anonima  
+    tecla.onclick = function () { 
         tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (e) {
+
+        if (e.code === 'Space' || e.code === 'Enter') {
+            tecla.classList.add ("ativa");
+        }
+
+    }
+
+    tecla.onkeyup = function (e) {
+        tecla.classList.remove ("ativa")
     }
 }
 
